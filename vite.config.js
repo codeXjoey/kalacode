063df0ee868b644e0ade3bbb70529d8214118a48
Maximiliano Santana <iimaxsany@gmail.com>
2023-09-15 09:41:58 -0600

    Errased woekflows

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [glsl()],
-  base: 'https://maximiliano-santana.github.io/github-pages',
   // build: [
   //   {
   //     from: '/resources/textures',
