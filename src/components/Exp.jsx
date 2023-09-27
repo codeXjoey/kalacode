0dac5d01687df16fd37ce34d9cd753ad6c8ed2f2
Maximiliano Santana <iimaxsany@gmail.com>
2023-09-27 16:19:27 -0600

    deleted textures and changed particles animation

--- src/components/Exp.jsx
+++ src/components/Exp.jsx
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import gsap from 'gsap';
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Timeline } from 'gsap/gsap-core';
 const Exp = () => {
   useEffect(()=>{
 
@@ -140,7 +141,7 @@ function initExperience (){
       fingerprintBaseColor3: '#1d5ef7',
       fingerprintWidth: 10,
       fingerprintHeight: 14,
-      fingerprintResolution: 110,
+      fingerprintResolution: 80,
       fingerprintParticlesSize : 70,
       fingerprintparticlesRandomOffset: 0.3,
     
@@ -159,6 +160,9 @@ function initExperience (){
     let fingerprintParticlesMaterial = null;
     let fingerprintParticlesGeometry = null;
     let fingerprintParticles = null;
+
+    let fingerprintParticlesBackMaterial = null;
+    let fingerprintParticlesBack = null;
   
     let randomParticlesGeometry = null;
     let randomParticlesMaterial = null;
@@ -245,6 +249,7 @@ function initExperience (){
   
     //Fingerprint
     const fingerprintTexture = textureLoader.load('/textures/fingerprint2.jpg');
+
   
     //QrCodes
     const qrCodesTextures = [];
@@ -408,6 +413,7 @@ function initExperience (){
     let scrollVelocity = null;
     window.addEventListener('scroll', (event)=>{
       gsap.to(timeLine, {duration:1, delay: 0, t: scrollY*scrollVelocity});
+      console.log(timeLine.t)
       if(isObjectSelected){
           qrCodeToPrevious();
       }
@@ -432,6 +438,8 @@ function initExperience (){
       previousTime = elapsedTime;
     
       //Animating Particles Position
+
+
       // if(timeLine.t > 400){
       //     randomParticles.position.z = (timeLine.t - 400)*0.25;
       // }
@@ -486,8 +494,9 @@ function initExperience (){
     
       //Update materials 
       fingerprintParticlesMaterial.uniforms.uTimeLine.value = timeLine.t;
-      fingerprintParticlesMaterial.uniforms.uTime.value = elapsedTime;
-    
+
+      fingerprintParticlesBackMaterial.uniforms.uTimeLine.value = timeLine.t;
+      
       randomParticlesMaterial.uniforms.uTime.value = elapsedTime;
       randomParticlesMaterial.uniforms.uTimeLine.value = timeLine.t;
     
@@ -594,7 +603,7 @@ function initExperience (){
           
               vertexPosition.z -= x;
               vertexPosition.z = clamp(vertexPosition.z, originalPosition, 1000.0);
-              vertexPosition.z += uTimeLine*0.04;
+              vertexPosition.z += uTimeLine*0.05;
               // vertexPosition.z += 50.0;
           
           
@@ -667,6 +676,114 @@ function initExperience (){
       fingerprintParticles.position.z = -40;
     
       scene.add(fingerprintParticles);
+
+      fingerprintParticlesBackMaterial = new THREE.ShaderMaterial({
+        vertexShader: `
+        attribute vec3 aRandomColors;
+        attribute float aRandom;
+    
+        uniform sampler2D uTexture;
+        uniform float uWidth;
+        uniform float uHeight;
+        uniform float uPointSize;
+        uniform float uPixelRatio;
+        uniform float uTime;
+        uniform float uTimeLine;
+    
+        varying vec3 vRandomColor;
+        varying vec4 vPointColor;
+    
+    
+        void main (){
+            vec3 vertexPosition = position;
+        
+            //Create animation
+            float originalPosition = position.z;
+            float randomDistance = aRandom*2.0;
+            float frameOrigin = 300.0;
+            float newTimeLine = abs(uTimeLine-frameOrigin)*-1.0+frameOrigin;
+            // float newTimeLine = (pow((uTimeLine-frameOrigin)*0.1, 3.0)*+100.0);
+            float x = newTimeLine*0.5; //-5.0 used to make the values of x can go bellow 0
+        
+            //Aplicando randomicidad a los vertices (posicion inicial)
+            vertexPosition.z += randomDistance*50.0+40.0;
+        
+        
+            vertexPosition.z -= x;
+            vertexPosition.z = clamp(vertexPosition.z, originalPosition, 1000.0);
+            vertexPosition.z -= uTimeLine*0.05;
+            // vertexPosition.z += 50.0;
+        
+        
+            vec4 modelPosition = modelMatrix * vec4(vertexPosition, 1.0);
+            vec4 viewPosition = viewMatrix * modelPosition;
+            vec4 projectedPosition = projectionMatrix * viewPosition;
+        
+            gl_Position = projectedPosition;
+        
+            //PointSize
+            gl_PointSize = uPointSize;
+        
+            //Perspective & pixelRatio fix
+            gl_PointSize *= clamp(1.0/ -viewPosition.z, 0.0, 0.25);
+            gl_PointSize *= uPixelRatio;
+        
+        
+        
+            //Varyings
+            vRandomColor = aRandomColors;
+        
+            vec2 vertexUv = vec2((vertexPosition.x*-1.0)/uWidth, vertexPosition.y/uHeight);
+            vertexUv.x += 0.5;
+            vertexUv.y += 0.5;
+        
+            vPointColor = texture2D(uTexture, vertexUv); 
+            vPointColor -= vec4(step(vPointColor.r, 0.5));
+        }`,
+        fragmentShader: `
+        varying vec3 vRandomColor;
+        varying vec4 vPointColor;
+      
+      
+        void main (){
+        
+            float distanceToCenter = distance(gl_PointCoord, vec2(0.5));
+            float strength = 0.05 / distanceToCenter - 0.1;
+        
+            gl_FragColor = vec4(vRandomColor.r * strength, vRandomColor.g * strength, vRandomColor.b* strength, strength );
+        
+        
+            vec4 color = vec4(vRandomColor.r*vPointColor.r, vRandomColor.g*vPointColor.g, vRandomColor.b*vPointColor.b, vPointColor.a);
+        
+            vec4 colorGlowPoint = vec4(color.r * strength, color.g * strength, color.b * strength, color.a * strength);
+        
+            gl_FragColor = vec4(color);
+        
+            // if(gl_FragColor.r < 0.1){
+            //     discard;
+            // }
+        
+        
+        }
+        `,
+        transparent: true,
+        depthWrite: false,
+        uniforms:{
+            uPointSize: { value: size },
+            uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) },
+            uTexture: { value: fingerprintTexture },
+            uWidth: { value: width },
+            uHeight: { value: height },
+            uTime: { value: 0 },
+            uTimeLine: { value: 0 },
+        }
+    }) ;
+
+      fingerprintParticlesBack = fingerprintParticles.clone();
+      fingerprintParticlesBack.material = fingerprintParticlesBackMaterial;
+      fingerprintParticlesBack.rotation.y = Math.PI;
+      scene.add(fingerprintParticlesBack);
+      
     }
   
     function createRandomParticles(){
